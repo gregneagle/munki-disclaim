6da3847fc65f4119459e7edf821a1fe3d6456385
Per Olofsson <magervalp@fastmail.fm>
2023-02-10 09:38:19 +0100

    Attempt conversion to Objective-C

--- munkishim.mm
+++ munkishim.mm
@@ -1,101 +1,138 @@
-#include <libgen.h>
 #include <signal.h>
 #include <spawn.h>
-#include <string>
 #include <unistd.h>
+#include <sysexits.h>
+#import <Foundation/Foundation.h>
 
-extern "C" {
-    int responsibility_spawnattrs_setdisclaim(posix_spawnattr_t attrs, int disclaim)
-        __attribute__((availability(macos,introduced=10.14), weak_import));
+
+int responsibility_spawnattrs_setdisclaim(posix_spawnattr_t attrs, int disclaim)
+    __attribute__((availability(macos,introduced=10.14), weak_import));
+
+// Category for NSArray that returns a plain C array of char * from an
+// NSArray with NSStrings
+@interface NSArray (CArrayCategory)
+
+- (char **)getCArray;
+
+@end
+
+@implementation NSArray (CArrayCategory)
+
+- (char **)getCArray
+{
+    NSUInteger count = [self count];
+    char **array = (char **)malloc((count + 1) * sizeof(char *));
+
+    for (unsigned i = 0; i < count; i++) {
+         array[i] = strdup([[self objectAtIndex:i] UTF8String]);
+    }
+    array[count] = NULL;
+    return array;
+}
+
+@end
+
+
+NSString *shimmedFlg = @"--shimmed";
+NSString *munkiPythonPath = @"/usr/local/munki/munki-python";
+
+NSArray *allowedCmds = @[
+    @"appusaged",
+    @"app_usage_monitor",
+    @"authrestartd",
+    @"launchapp",
+    @"logouthelper",
+    @"managedsoftwareupdate",
+    @"supervisor"
+];
+
+
+int execPython(NSArray<NSString *> *args) {
+    // check command is allowed
+    NSString *cmd = [args[0] lastPathComponent];
+    if (! [args containsObject:cmd]) {
+        printf("Unknown cmd: %s\n", args[0].UTF8String);
+        exit(EPERM);
+    }
+
+    // FIXME: This is the wrong approach
+    NSString *absPath = [NSString stringWithFormat:@"/usr/local/munki/%@", cmd];
+    if (! [args[1] isEqualToString:absPath]) {
+        printf("Unknown path: %s\n", args[0].UTF8String);
+        exit(EPERM);
+    }
+
+    // copy args and replace ".../{cmd} --shimmed" with ".../munki-python .../{cmd}.py"
+    NSMutableArray *newArgs = [args mutableCopy];
+    [newArgs removeObjectAtIndex:0];
+    [newArgs replaceObjectAtIndex:0 withObject:[NSString stringWithFormat:@"%@.py", args[0]]];
+
+    char **new_argv = [newArgs getCArray];
+    if (execvp(new_argv[0], &new_argv[0]) == -1) {
+        return errno;
+    }
+    return 0;
 }
 
 #define POSIX_CHECK(expr) \
-    if (int err = (expr)) { \
+    if ((err = (expr))) { \
         exit(err); \
     }
 
-const char *shimmed_flg = "--shimmed";
-const char *munki_python_path = "/usr/local/munki/munki-python";
-
-const std::string allowed_cmds[] = {"appusaged", "app_usage_monitor", "authrestartd", "launchapp", "logouthelper", "managedsoftwareupdate", "supervisor"};
-
-int main(int argc, char * const argv[], char *const *envp) {
-    // if shimmed, exec python
-    if (argc > 1 && strcmp(argv[1], shimmed_flg) == 0) {
-        // check command is allowed    
-        char *cmd = basename(strdup(argv[0]));
-        if (std::find(std::begin(allowed_cmds), std::end(allowed_cmds), cmd) == std::end(allowed_cmds)) {    
-            printf("Unknown cmd: %s\n", argv[0]);     
-            exit(EPERM);    
-        }
-
-        // check path is absolute
-        char *abs_path;
-        asprintf(&abs_path, "/usr/local/munki/%s", cmd);
-        if (strcmp(argv[0], abs_path) != 0) {
-            printf("Unknown path: %s\n", argv[0]);     
-            exit(EPERM);    
-        }
-
-        // copy args and replace ".../{cmd} --shimmed" with ".../munki-python .../{cmd}.py"
-        char **new_argv = (char **)malloc((argc+1) * sizeof(char *));
-        new_argv[0] = strdup(munki_python_path);
-        char *new_path;
-        asprintf(&new_path, "%s.py", argv[0]);
-        new_argv[1] = new_path;
-        if (argc > 2) {
-            for (int i = 2; i <= argc; i++)
-                new_argv[i] = argv[i];
-        }
-
-        // exec python script
-        if (execvp(new_argv[0], &new_argv[0]) == -1) {
-            return errno;
-        }
-        return 0;
-    }
-    // otherwise exec shimmed self
-     
+int execShimmed(NSArray<NSString *> *args, char *const *envp) {
+    int err;
+    
     // set argv to "--shimmed" + argv
-    char **new_argv = (char **)malloc((argc+2) * sizeof(char *));
-    new_argv[0] = strdup(argv[0]);
-    new_argv[1] = strdup(shimmed_flg);
-    if (argc > 1) {
-        for (int i = 1; i <= argc; i++)
-            new_argv[i+1] = argv[i];
-    }
+    NSMutableArray *newArgs = [args mutableCopy];
+    [newArgs insertObject:shimmedFlg atIndex:1];
+    char **new_argv = [newArgs getCArray];
     
     // init posix attr
     posix_spawnattr_t attr;
     POSIX_CHECK(posix_spawnattr_init(&attr));
-
+    
     // act like execve(2)
     short flags = POSIX_SPAWN_SETEXEC;
-
+    
     // reset signal mask
     sigset_t sig_mask;
     sigemptyset(&sig_mask);
     POSIX_CHECK(posix_spawnattr_setsigmask(&attr, &sig_mask));
     flags |= POSIX_SPAWN_SETSIGMASK;
-
+    
     // reset signals to default behavior
     sigset_t sig_default;
     sigfillset(&sig_default);
     POSIX_CHECK(posix_spawnattr_setsigdefault(&attr, &sig_default));
     flags |= POSIX_SPAWN_SETSIGDEF;
-
+    
     // set flags
     POSIX_CHECK(posix_spawnattr_setflags(&attr, flags));
-
+    
     // force TCC responsibility on child
-    if (@available(macOS 10.14, *))
+    if (@available(macOS 10.14, *)) {
         POSIX_CHECK(responsibility_spawnattrs_setdisclaim(&attr, 1));
-
+    }
+    
     // exec shimmed process
-    int err = posix_spawn(NULL, argv[0], NULL, &attr, new_argv, envp);
-
+    err = posix_spawn(NULL, new_argv[0], NULL, &attr, new_argv, envp);
+    
     // clean up attr
     posix_spawnattr_destroy(&attr);
-
+    
     return err;
+}
+
+
+int main(int argc, char * const argv[], char *const *envp) {
+    NSArray<NSString *> *args = [[NSProcessInfo processInfo] arguments];
+    
+    // If we're called with --shimmed the child has been disclaimed and we
+    // execute python with the original command, dropping --shimmed.
+    if (args.count > 1 && [args[1] isEqualToString:shimmedFlg]) {
+        return execPython(args);
+    } else {
+        // Otherwise we call the disclaim logic and add a --shimmed argument.
+        return execShimmed(args, envp);
+    }
 }
